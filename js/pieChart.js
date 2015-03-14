806605967222cfc8844e8d4a8ac29e9bb7b4118f
Felicia Ringblom <felicia.ringblom@gmail.com>
2015-03-14 23:42:24 +0100

    added font-awesome + style changes in water section

--- js/pieChart.js
+++ js/pieChart.js
@@ -4,7 +4,8 @@ var w_div= "#water_pie_chart";
 var w = $(w_div).width();
 var h = w;
 var r = h/2;
-var color = d3.scale.category20c();
+
+var color = ["#56A3BF", "#3A6D7F", "#73DAFF","#1D3640", "#68C4E5"];
 
     //data from SCB, from the data/waterUseSweden.csv file
     var data = [{"label":"hushåll", "value":575640}, 
@@ -16,16 +17,19 @@ var color = d3.scale.category20c();
                 .append("svg:svg").data([data])
                 .attr("width", w).attr("height", h)
                 .append("svg:g").attr("transform", "translate(" + r + "," + r + ")");
+    
     var pie = d3.layout.pie().value(function(d){return d.value;});
 
     // declare an arc generator function
-    var arc = d3.svg.arc().outerRadius(r);
+    var arc = d3.svg.arc()
+    .outerRadius(r * 0.8)
+    .innerRadius(r * 0.4);
 
     // select paths, use arc generator to draw
     var arcs = vis.selectAll("g.slice").data(pie).enter().append("svg:g").attr("class", "slice");
     arcs.append("svg:path")
         .attr("fill", function(d, i){
-            return color(i);
+            return color[i];
         })
         .attr("d", function (d) {
             return arc(d);
