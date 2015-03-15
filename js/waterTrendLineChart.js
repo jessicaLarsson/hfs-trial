348c869d4bdf4f379bbac5dc95391e40a602e888
Felicia Ringblom <felicia.ringblom@gmail.com>
2015-03-15 02:54:15 +0100

    adjusted position ov körsträcka label

--- js/waterTrendLineChart.js
+++ js/waterTrendLineChart.js
@@ -17,13 +17,9 @@ function waterTrendLineChart(data) {
 		      .append("svg:g")
 		      .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
 
-	 	// create yAxis
-	 	var xAxis = d3.svg.axis().scale(x).tickSize(5).tickSubdivide(false);
-
 		graph.append("svg:g")
-		      .attr("class", "x axis")
-		      .attr("transform", "translate(0," + h + ")")
-		      .call(xAxis);
+		      .attr("class", "x axis label_fix")
+		      .attr("transform", "translate(0," + h + ")");
 
 
 	 	var yAxisLeft = d3.svg.axis().scale(y).ticks(5).orient("left");
@@ -36,6 +32,76 @@ function waterTrendLineChart(data) {
 		graph.append("svg:path")
 			.attr("d", line(data))
 			.attr("class", "line_graph_path");
+
+
+
+		var label1 = d3.select(".label_fix")
+					.append("g")
+					.attr("class", "tick")
+					.attr("transform", "translate(0, 0)");
+
+				label1.append("text")
+					.attr("dy", ".71em")
+					.attr("y", 4)
+					.attr("x", 0)
+					.attr("style", "text-anchor: middle;")
+					.text(1995);
+
+				label1.append("line")
+					.attr("y2", 5)
+					.attr("x2", 0)
+					.attr("style", "stroke:rgb(0,0,0);")
+
+		var label2 = d3.select(".label_fix")
+					.append("g")
+					.attr("class", "tick")
+					.attr("transform", "translate(90, 0)");
+
+				label2.append("text")
+					.attr("dy", ".71em")
+					.attr("y", 4)
+					.attr("x", 0)
+					.attr("style", "text-anchor: middle;")
+					.text(2000);
+
+				label2.append("line")
+					.attr("y2", 5)
+					.attr("x2", 0)
+					.attr("style", "stroke:rgb(0,0,0);")
+
+		var label3 = d3.select(".label_fix")
+					.append("g")
+					.attr("class", "tick")
+					.attr("transform", "translate(180, 0)");
+					
+				label3.append("text")
+					.attr("dy", ".71em")
+					.attr("y", 4)
+					.attr("x", 0)
+					.attr("style", "text-anchor: middle;")
+					.text(2005);
+
+				label3.append("line")
+					.attr("y2", 5)
+					.attr("x2", 0)
+					.attr("style", "stroke:rgb(0,0,0);")
+
+		var label4 = d3.select(".label_fix")
+					.append("g")
+					.attr("class", "tick")
+					.attr("transform", "translate(270, 0)");
+
+				label4.append("text")
+					.attr("dy", ".71em")
+					.attr("y", 4)
+					.attr("x", 0)
+					.attr("style", "text-anchor: middle;")
+					.text(2010);
+
+				label4.append("line")
+					.attr("y2", 5)
+					.attr("x2", 0)
+					.attr("style", "stroke:rgb(0,0,0);")
 
 	this.update = function(data){
 		
